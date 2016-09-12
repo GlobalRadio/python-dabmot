d72a533822203ecbffc5506768987d82ddf83b0a
Ben Poor <ben.poor@thisisglobal.com>
2016-09-12 19:04:13 +0100

    packaging for pypi

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 setup(name='dabmot',
-      version='1.0.0',
+      version='1.0.1',
       description='DAB MOT object assembly and decoding',
       author='Ben Poor',
       author_email='ben.poor@thisisglobal.com',
       url='https://github.com/GlobalRadio/python-dabmot',
-      download_url='https://github.com/GlobalRadio/python-dabmot/tarball/1.0.0',
+      download_url='https://github.com/GlobalRadio/python-dabmot/tarball/1.0.1',
       packages=['mot'],
       package_dir = {'' : 'src'},
       keywords = ['dab', 'mot', 'radio']
